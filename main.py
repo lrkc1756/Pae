a84e27bc351eb46a19ffc5fbeeb908af1776145c
lrkc1756 <142984076+lrkc1756@users.noreply.github.com>
2025-04-14 15:29:18 +0200

    fix: reconnect style.css. Make sure that the screen is zoomed to 100% so css loads correctly

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-from website import create_app 
+import os
+from website import create_app
 
 app = create_app()
 
-#runs webserver if this file is run directly
 if __name__ == '__main__':
-    app.run(host='192.168.1.103', port=5000, debug=True)
-    
-    
+    app.run(
+        host=os.environ.get('HOST', '127.0.0.1'),
+        port=int(os.environ.get('PORT', 5000)),
+        debug=True
+    )
