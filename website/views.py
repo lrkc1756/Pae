896945335dd4c7c88f7a49485711d40ad876d06b
lrkc1756 <142984076+lrkc1756@users.noreply.github.com>
2025-04-15 17:22:40 +0200

    clean: remove code from Note tutorial

--- website/views.py
+++ website/views.py
@@ -17,18 +17,6 @@ def home():
         {"name": "Plastic Recycling", "description": "Einarbeitung von Produktions-abfall in neue Teile", "image": "plastic_recycling.png"}, 
         {"name": "", "description": "", "image":"logo.png"}
     ]
-        
-    if request.method == 'POST': 
-        note = request.form.get('note')#Gets the note from the HTML 
-        #category = request.form.get('category')
-
-        if len(note) < 1:
-            flash('Note is too short!', category='error') 
-        else:
-            new_note = Note(data=note, user_id=current_user.id)  #providing the schema for the note , category=category
-            db.session.add(new_note) #adding the note to the database 
-            db.session.commit()
-            flash('Note added!', category='success')
 
     return render_template("home.html", demonstrator=demonstrator, user=current_user)
 
@@ -53,16 +41,3 @@ def about():
 @views.route('/co2')
 def co2():
     return render_template("co2.html", user=current_user)
-
-
-@views.route('/delete-note', methods=['POST'])
-def delete_note():  
-    note = json.loads(request.data) # this function expects a JSON from the INDEX.js file 
-    noteId = note['noteId']
-    note = Note.query.get(noteId)
-    if note:
-        if note.user_id == current_user.id:
-            db.session.delete(note)
-            db.session.commit()
-
-    return jsonify({})
